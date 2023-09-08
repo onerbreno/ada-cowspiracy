01e5da661fe76d1661a0414a42791f99acc9a306
Breno <onerbreno@outlook.com>
2023-09-08 09:50:59 -0300

    refactor Change bet command

--- src/commands/apostar.js
+++ src/commands/apostar.js
@@ -2,23 +2,23 @@ import Discord from 'discord.js'
 
 export const command = {
   name: 'apostar',
-  description: 'Aposta dinheiro em um número sorteado. \nRecebe **dinheiro** e **número da sorte**.',
+  description: 'Aposta dinheiro em um número sorteado de 1 a 5. \nRecebe **dinheiro** e **número da sorte**.',
 
   async run({ client, message, userAccount, args }) {
-    const amount = Number(args[0])
-    const userSelectedNumber = args[1]
+    const userSelectedNumber = Number(args[0])
+    const amount = Number(args[1])
 
-    const response = userAccount.placeBet(amount, userSelectedNumber)
+    const response = userAccount.placeBet({ userSelectedNumber, amount })
 
     message.channel.send({
       embeds: [{
         color: Discord.resolveColor('8B09E5'),
         author: {
-          name: response.name,
-          icon_url: response.avatarUrl
+          name: userAccount.name,
+          icon_url: userAccount.avatarUrl
         },
         description: response,
       }]
-    });
+    })
   }
 }
